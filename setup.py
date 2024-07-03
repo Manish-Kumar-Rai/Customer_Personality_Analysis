9cf33dc277e2c8bc870118d90efaf999f7a7f631
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2024-07-03 14:45:14 +0530

    'final'

--- setup.py
+++ setup.py
@@ -1,30 +1,25 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HYPEN_E_DOT='-e .'
 
-hypen_e_dot = "-e ."
-def get_requirments(file_path:str)->List[str]:
-    """
-        This function gets all the libraries inside the requirements file
-    """
-    requirements = []
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n","")for req in requirements]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if hypen_e_dot in requirements:
-            requirements.remove(hypen_e_dot)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
 
-
 setup(
-    name = "Customer_Personality_Analysis",
-    version="0.0.1",
-    author= "Manish kumar Rai",
-    author_email= "manishkumarrai3389@gamil.com",
-    packages= find_packages(),
-    install_requires = get_requirments('requirements.txt')
-
+    name='Customer personality',
+    version='0.0.1',
+    author='Shashank Shukla',
+    author_email='Shuklashashank297@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
 )
